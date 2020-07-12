b8a89dd1e0e0fe30867675dec322918bc22e11f4
madiha-dev <50271912+madiha-dev@users.noreply.github.com>
2020-07-12 17:57:03 +0000

    Update index.js. convert it to Room project
    Here need to creat new project, but i didn't. Just Gate and study components are no more in use.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,62 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Study from './Study';
+// import Gate from './Gate';
+// import './index.css';
+
+// /*What makes a fun, is a component??
+// - fun has JSX syntax, when react invoke the fun,
+// it gets JSX, and renders the equivalent HTML o the DOM
+// */
+
+// function Hi(props){
+//     return (
+//     <div>
+//         <p>HI! this is <strong>{props.name}.</strong></p>
+//         <h4>Learning React</h4>
+//         <ul>
+//             <li>Project 1n</li>
+//             <li>Project 2</li>
+//             <li>Project 3</li>
+//         </ul>
+        
+//     </div>
+//     );
+// }
+
+// /*Rendering
+// - call the fun e.g. here is "Hi"
+// - gets JSX
+// - insert HTML elements with id "#root" into
+//      document.querySelector("#root")
+// */
+// ReactDOM.render(
+// [
+// <Hi 
+//     name="madiha"/>, 
+// <Study 
+//     name='React Practice'
+//     url='/images/PK.jpeg'/>,
+// <Gate
+//     isOpen='true'/>,
+//     <Gate
+//     isOpen='false'/>
+// ], 
+// document.querySelector('#root'));
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Study from './Study';
-import Gate from './Gate';
-import './index.css';
 
-/*What makes a fun, is a component??
-- fun has JSX syntax, when react invoke the fun,
-it gets JSX, and renders the equivalent HTML o the DOM
-*/
-
-function Hi(props){
+function Room() {
     return (
-    <div>
-        <p>HI! this is <strong>{props.name}.</strong></p>
-        <h4>Learning React</h4>
-        <ul>
-            <li>Project 1n</li>
-            <li>Project 2</li>
-            <li>Project 3</li>
-        </ul>
-        
-    </div>
+        <div className='room'>The room is lit</div>
     );
 }
 
-/*Rendering
-- call the fun e.g. here is "Hi"
-- gets JSX
-- insert HTML elements with id "#root" into
-     document.querySelector("#root")
-*/
-ReactDOM.render(
-[
-<Hi 
-    name="madiha"/>, 
-<Study 
-    name='React Practice'
-    url='/images/PK.jpeg'/>,
-<Gate
-    isOpen='true'/>,
-    <Gate
-    isOpen='false'/>
-], 
-document.querySelector('#root'));
+ReactDOM.render(<Room/>, document.getElementById('root'));
+
+/*document.getElementById with root
+ same like 
+ document.querySelector with #root
+ */
