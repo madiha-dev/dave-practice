4e73c329dd1d50573d623956454108dfe85c1230
madiha-dev <50271912+madiha-dev@users.noreply.github.com>
2020-07-12 10:25:37 +0000

    What makes a fun, is a component?

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*What makes a fun, is a component??
+- fun has JSX syntax, when react invoke the fun,
+it gets JSX, and renders the equivalent HTML o the DOM
+*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Hi(){
+    return (
+    <div>
+        <p>HI! thiss is <strong>madiha</strong></p>
+    </div>
+    );
+}
+
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
